2cf2359e062c8f79fd98604b09d68cd28dbe55df
ddudukda <dk.kwon094@gmail.com>
2022-07-08 00:43:08 +0900

    #6 - 스프링부트 프로젝트 시작 들어간 기술 - 스프링웹 - 엑츄에이터 - 데브툴즈 - 롬복

---
+++ fastcampus-project-spingboard/src/main/java/com/fastcampus/projectspingboard/FastCampusProjectSpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectspingboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectSpringBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectSpringBoardApplication.class, args);
+    }
+
+}
