a875ddb6de01d3de252867ba1e48532a92b89359
ddudukda <dk.kwon094@gmail.com>
2022-08-02 17:13:40 +0900

    #32-게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

--- fastcampus-project-spingboard/src/main/java/com/fastcampus/projectspingboard/dto/ArticleDto.java
+++ fastcampus-project-spingboard/src/main/java/com/fastcampus/projectspingboard/dto/ArticleDto.java
@@ -14,12 +14,18 @@ public record ArticleDto(
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
-        String modifiedBy )
-{
+        String modifiedBy
+) {
+
+    public static ArticleDto of(UserAccountDto userAccountDto, String title, String content, String hashtag) {
+        return new ArticleDto(null, userAccountDto, title, content, hashtag, null, null, null, null);
+    }
+
     public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
     }
-    public static ArticleDto from(Article entity){
+
+    public static ArticleDto from(Article entity) {
         return new ArticleDto(
                 entity.getId(),
                 UserAccountDto.from(entity.getUserAccount()),
@@ -29,15 +35,17 @@ public record ArticleDto(
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),
                 entity.getModifiedAt(),
-                entity.getModifiedBy() );
+                entity.getModifiedBy()
+        );
     }
 
-    public Article toEntity() {
+    public Article toEntity(UserAccount userAccount) {
         return Article.of(
-                userAccountDto.toEntity(),
+                userAccount,
                 title,
                 content,
                 hashtag
         );
     }
+
 }
